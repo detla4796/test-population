bd1a122a5bb5dbc1f77fc1a2d0734a5ac563b601
Danil Ushakov <danilusakov16@gmail.com>
2025-02-05 12:33:08 +0500

    Updated code

--- Population/Population.cpp
+++ Population/Population.cpp
@@ -1,9 +1,10 @@
 ﻿#include <iostream>
+#include <filesystem>
 #include <fstream>
 #include <vector>
 #include <string>
-
 using namespace std;
+
 int main()
 {
 	ifstream f1("Population.txt");
@@ -32,7 +33,7 @@ int main()
 
 	for (int i = 65; i < 91; i++)
 	{
-		double smp{ 0 };
+		long long smp{ 0 };
 		int count = 0;
 		int k = 0;
 		string strana;
@@ -42,8 +43,8 @@ int main()
 			if ((strana[0] >= 48) && (strana[0] <= 57))
 			{
 				count++;
-				smp += stod(strana);
-				if (stod(strana) > 100000000)
+				smp += stol(strana);
+				if (stol(strana) > 100000000)
 				{
 					k++;
 				}
@@ -52,7 +53,7 @@ int main()
 		ofstream alphabet_out("Country\\" + string(1, char(i)) + ".txt", ios::app);
 		alphabet_out << "\nTotal population: " << smp << endl;
 		alphabet_out << "\nNumber of countries: " << count << endl;
-		alphabet_out << "\nAverage population: " << smp / count << endl;
-		alphabet_out << "\nNumber of countries with a population of more than 100 million: " << k << endl;
+		alphabet_out << "\nAverage population: " << (count ? smp / count : 0) << endl;
+		alphabet_out << "\nNumber of countries with a population of more than 100 million: " << k;
 	}
 }
